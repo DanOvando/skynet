56737059034477d5fc5a3ca8b34199d8a74e9954
DanOvando <dovando@bren.ucsb.edu>
2017-07-23 22:38:21 -0700

    query erddap now works

--- functions/query_erddap.R
+++ functions/query_erddap.R
@@ -1,3 +1,20 @@
+#' Title
+#'
+#' @param desired_data the data required, options 'sst', 'chl-a'
+#' @param runit the rounding unit for lat lon, .25 means rounded to x.25 lat/lon
+#' @param min_year the first year desired
+#' @param max_year the last year desired
+#' @param date_interval the interval of the dates required very database specific
+#' @param min_lat min lat
+#' @param max_lat max lat
+#' @param min_lon min lon
+#' @param max_lon max lon
+#' @param space_interval the interval of lat/lon to hunt over- very database specific
+#'
+#' @return
+#' @export
+#'
+#' @examples
 query_erddap <- function(desired_data = 'sst',
                          runit = 0.25,
                          min_year,
@@ -8,36 +25,62 @@ query_erddap <- function(desired_data = 'sst',
                          min_lon,
                          max_lon,
                          space_interval = .25) {
-
-  year_query <- paste0('[(',min_year,'-01-01):',date_interval,':(',max_year,'-12-31)]')
-
-  lat_query <- paste0('[(',min_lat,'):',100*space_interval,':(', max_lat,')]')
-
-  lon_query <- paste0('[(',min_lon,'):',100*space_interval,':(', max_lon,')]')
-
-  query <- paste0(year_query,lat_query,lon_query)
-
   if (desired_data == 'sst') {
+    year_query <-
+      paste0('[(',
+             min_year,
+             '-01-01):',
+             date_interval,
+             ':(',
+             max_year,
+             '-12-31)]')
+
+    lat_query <-
+      paste0('[(', min_lat, '):', space_interval, ':(', max_lat, ')]')
+
+    lon_query <-
+      paste0('[(', min_lon, '):', space_interval, ':(', max_lon, ')]')
+
+    query <- paste0(year_query, lat_query, lon_query)
+
     dat <-
       jsonlite::fromJSON(
-        paste0('https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.json?analysed_sst',query),
+        paste0(
+          'https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.json?analysed_sst',
+          query
+        ),
         flatten = T
       )
-      # paste0('https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.json?analysed_sst[(2012-01-01):14:(2013-12-29)][(53.00):25:(66.00)][(-179.99):25:(-157.00)]',
+    # paste0('https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.json?analysed_sst[(2012-01-01):14:(2013-12-29)][(53.00):25:(66.00)][(-179.99):25:(-157.00)]',
 
   }
-  if (desired_data == 'chl-a'){
+  if (desired_data == 'chl-a') {
+    year_query <-
+      paste0('[(',
+             min_year,
+             '-01-01):',
+             date_interval,
+             ':(',
+             max_year,
+             '-12-31)]')
 
-    # durl <- "http://upwell.pfeg.noaa.gov/erddap/griddap/erdMH1chlamday.json?chlorophyll[(2010-06-01):1:(2010-10-01)][(66):1:(53)][(-180):1:(-157)]"
+    lat_query <-
+      paste0('[(', max_lat, '):', space_interval, ':(', min_lat, ')]') # for some unholy reason the lats go in reverse for this database
 
+    lon_query <-
+      paste0('[(', min_lon, '):', space_interval, ':(', max_lon, ')]')
+
+    query <- paste0(year_query, lat_query, lon_query)
     dat <-
       jsonlite::fromJSON(
-        paste0('http://upwell.pfeg.noaa.gov/erddap/griddap/erdMH1chlamday.json?chlorophyll',query)
+        paste0(
+          'http://upwell.pfeg.noaa.gov/erddap/griddap/erdMH1chlamday.json?chlorophyll',
+          query
+        )
 
-,        flatten = T
+        ,
+        flatten = T
       )
-
-
 
   }
   datnames <- dat$table$columnNames
